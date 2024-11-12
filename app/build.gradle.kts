987fcc02f65ceeade7a1c6b744bdfb944c7662dc
ZackMvP <mcpc.mvp@gmail.com>
2024-11-13 00:14:57 +0700

    Added DAO

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,10 @@ android {
 
 dependencies {
     // Import the Compose BOM
+    //Room
+    implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
+    ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
+    implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.compose.material3:material3")
